a319e90806bf9a5685d51f76e3bfec8c272aef51
prikolov17 <prikolov17@gmail.com>
2025-10-03 20:30:49 +0300

    Add files via upload

---
+++ lab_2/1.3.py
@@ -0,0 +1,6 @@
+def max_of_two(x, y):
+    if x>y:
+        return x
+    else:
+        return y
+print(max_of_two(11,3))
---
+++ lab_2/2.py
@@ -0,0 +1,4 @@
+def describe_person(name, age=30):
+    print(f'Имя:{name} Возраст:{age}')
+describe_person('Виктор',30)
+describe_person('Алекс',30)
